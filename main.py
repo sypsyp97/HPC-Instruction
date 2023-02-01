0f9c0634fd11bd3bf79b94496f394aff4c2b9865
liuaijia <ge95hul@mytum.de>
2023-02-01 17:15:40 +0100

    init

---
+++ main.py
@@ -0,0 +1,12 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import tensorflow as tf
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print(tf.config.list_physical_devices('GPU'))
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
