efc595d765858bd0e95a9d102fcf86c454ef1e3b
liuaijia <ge95hul@mytum.de>
2023-02-01 17:16:53 +0100

    init

--- main.py
+++ main.py
@@ -5,8 +5,10 @@
 import tensorflow as tf
 
 
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print(tf.config.list_physical_devices('GPU'))
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
